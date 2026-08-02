d6a4ec31699d4bc79a1d0d777326784203997837
askhmza-prime <askhmza@gmail.com>
2026-08-02 15:42:49 +0530

    Create site constants

---
+++ frontend/constants/navigation.ts
@@ -0,0 +1,26 @@
+export const NAVIGATION = [
+      {
+          label: "Home",
+              href: "/",
+                },
+                  {
+                      label: "About",
+                          href: "/about",
+                            },
+                              {
+                                  label: "Projects",
+                                      href: "/projects",
+                                        },
+                                          {
+                                              label: "Gallery",
+                                                  href: "/gallery",
+                                                    },
+                                                      {
+                                                          label: "Testimonials",
+                                                              href: "/testimonials",
+                                                                },
+                                                                  {
+                                                                      label: "Contact",
+                                                                          href: "/contact",
+                                                                            },
+                                                                            ];
